ea18f7bbec9a37b91a54b0440fe6f4d0af7d8fe4
Brad Risse <brad@omniholding.com>
2018-02-19 10:13:29 -0800

    Back to Test Net

--- dist/web.js
+++ dist/web.js
@@ -2570,98 +2570,23 @@ class TreeTransaction {
 Class.register(TreeTransaction);
 
 class BufferUtils {
-    static _codePointTextDecoder(u8) {
-        if (typeof TextDecoder === 'undefined') throw new Error('TextDecoder not supported');
-        if (BufferUtils._ISO_8859_15_DECODER === null) throw new Error('TextDecoder does not supprot iso-8859-15');
-        if (BufferUtils._ISO_8859_15_DECODER === undefined) {
-            try {
-                BufferUtils._ISO_8859_15_DECODER = new TextDecoder('iso-8859-15');
-            } finally {
-                BufferUtils._ISO_8859_15_DECODER = null;
-            }
-        }
-        return BufferUtils._ISO_8859_15_DECODER.decode(u8)
-            .replace('€', '¤').replace('Š', '¦').replace('š', '¨').replace('Ž', '´')
-            .replace('ž', '¸').replace('Œ', '¼').replace('œ', '½').replace('Ÿ', '¾');
-    }
-
-    static _tripletToBase64(num) {
-        return BufferUtils._BASE64_LOOKUP[num >> 18 & 0x3F] + BufferUtils._BASE64_LOOKUP[num >> 12 & 0x3F] + BufferUtils._BASE64_LOOKUP[num >> 6 & 0x3F] + BufferUtils._BASE64_LOOKUP[num & 0x3F];
-    }
-
-    static _base64encodeChunk(u8, start, end) {
-        let tmp;
-        const output = [];
-        for (let i = start; i < end; i += 3) {
-            tmp = ((u8[i] << 16) & 0xFF0000) + ((u8[i + 1] << 8) & 0xFF00) + (u8[i + 2] & 0xFF);
-            output.push(BufferUtils._tripletToBase64(tmp));
-        }
-        return output.join('');
-    }
-
-    static _base64fromByteArray(u8) {
-        let tmp;
-        const len = u8.length;
-        const extraBytes = len % 3; // if we have 1 byte left, pad 2 bytes
-        let output = '';
-        const parts = [];
-        const maxChunkLength = 16383; // must be multiple of 3
-
-        // go through the array every three bytes, we'll deal with trailing stuff later
-        for (let i = 0, len2 = len - extraBytes; i < len2; i += maxChunkLength) {
-            parts.push(BufferUtils._base64encodeChunk(u8, i, (i + maxChunkLength) > len2 ? len2 : (i + maxChunkLength)));
-        }
-
-        // pad the end with zeros, but make sure to not forget the extra bytes
-        if (extraBytes === 1) {
-            tmp = u8[len - 1];
-            output += BufferUtils._BASE64_LOOKUP[tmp >> 2];
-            output += BufferUtils._BASE64_LOOKUP[(tmp << 4) & 0x3F];
-            output += '==';
-        } else if (extraBytes === 2) {
-            tmp = (u8[len - 2] << 8) + (u8[len - 1]);
-            output += BufferUtils._BASE64_LOOKUP[tmp >> 10];
-            output += BufferUtils._BASE64_LOOKUP[(tmp >> 4) & 0x3F];
-            output += BufferUtils._BASE64_LOOKUP[(tmp << 2) & 0x3F];
-            output += '=';
-        }
-
-        parts.push(output);
-
-        return parts.join('');
-    }
-
-    /**
+     /**
      * @param {*} buffer
      * @return {string}
      */
     static toBase64(buffer) {
-        if (typeof Buffer !== 'undefined' && typeof window === 'undefined') {
-            return new Buffer(buffer).toString('base64');
-        } else if (typeof TextDecoder !== 'undefined' && BufferUtils._ISO_8859_15_DECODER !== null) {
-            try {
-                return btoa(BufferUtils._codePointTextDecoder(new Uint8Array(buffer)));
-            } catch (e) {
-                // Disabled itself
-            }
-        }
-
-        return BufferUtils._base64fromByteArray(new Uint8Array(buffer));
+        return btoa(String.fromCharCode(...new Uint8Array(buffer)));
     }
 
     /**
      * @param {string} base64
-     * @return {Uint8Array}
+     * @return {SerialBuffer}
      */
     static fromBase64(base64) {
         return Uint8Array.from(atob(base64), c => c.charCodeAt(0));
     }
 }
-BufferUtils.BASE64_ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
-BufferUtils._BASE64_LOOKUP = [];
-for (let i = 0, len = BufferUtils.BASE64_ALPHABET.length; i < len; ++i) {
-    BufferUtils._BASE64_LOOKUP[i] = BufferUtils.BASE64_ALPHABET[i];
-}
+
 Class.register(BufferUtils);
 
 class JSONUtils {
@@ -9230,72 +9155,11 @@ class BufferUtils {
      * @return {Uint8Array}
      */
     static fromAscii(string) {
-        const buf = new Uint8Array(string.length);
+        var buf = new Uint8Array(string.length);
         for (let i = 0; i < string.length; ++i) {
             buf[i] = string.charCodeAt(i);
         }
         return buf;
-    }
-
-    static _codePointTextDecoder(u8) {
-        if (typeof TextDecoder === 'undefined') throw new Error('TextDecoder not supported');
-        if (BufferUtils._ISO_8859_15_DECODER === null) throw new Error('TextDecoder does not supprot iso-8859-15');
-        if (BufferUtils._ISO_8859_15_DECODER === undefined) {
-            try {
-                BufferUtils._ISO_8859_15_DECODER = new TextDecoder('iso-8859-15');
-            } finally {
-                BufferUtils._ISO_8859_15_DECODER = null;
-            }
-        }
-        return BufferUtils._ISO_8859_15_DECODER.decode(u8)
-            .replace('€', '¤').replace('Š', '¦').replace('š', '¨').replace('Ž', '´')
-            .replace('ž', '¸').replace('Œ', '¼').replace('œ', '½').replace('Ÿ', '¾');
-    }
-
-    static _tripletToBase64(num) {
-        return BufferUtils._BASE64_LOOKUP[num >> 18 & 0x3F] + BufferUtils._BASE64_LOOKUP[num >> 12 & 0x3F] + BufferUtils._BASE64_LOOKUP[num >> 6 & 0x3F] + BufferUtils._BASE64_LOOKUP[num & 0x3F];
-    }
-
-    static _base64encodeChunk(u8, start, end) {
-        let tmp;
-        const output = [];
-        for (let i = start; i < end; i += 3) {
-            tmp = ((u8[i] << 16) & 0xFF0000) + ((u8[i + 1] << 8) & 0xFF00) + (u8[i + 2] & 0xFF);
-            output.push(BufferUtils._tripletToBase64(tmp));
-        }
-        return output.join('');
-    }
-
-    static _base64fromByteArray(u8) {
-        let tmp;
-        const len = u8.length;
-        const extraBytes = len % 3; // if we have 1 byte left, pad 2 bytes
-        let output = '';
-        const parts = [];
-        const maxChunkLength = 16383; // must be multiple of 3
-
-        // go through the array every three bytes, we'll deal with trailing stuff later
-        for (let i = 0, len2 = len - extraBytes; i < len2; i += maxChunkLength) {
-            parts.push(BufferUtils._base64encodeChunk(u8, i, (i + maxChunkLength) > len2 ? len2 : (i + maxChunkLength)));
-        }
-
-        // pad the end with zeros, but make sure to not forget the extra bytes
-        if (extraBytes === 1) {
-            tmp = u8[len - 1];
-            output += BufferUtils._BASE64_LOOKUP[tmp >> 2];
-            output += BufferUtils._BASE64_LOOKUP[(tmp << 4) & 0x3F];
-            output += '==';
-        } else if (extraBytes === 2) {
-            tmp = (u8[len - 2] << 8) + (u8[len - 1]);
-            output += BufferUtils._BASE64_LOOKUP[tmp >> 10];
-            output += BufferUtils._BASE64_LOOKUP[(tmp >> 4) & 0x3F];
-            output += BufferUtils._BASE64_LOOKUP[(tmp << 2) & 0x3F];
-            output += '=';
-        }
-
-        parts.push(output);
-
-        return parts.join('');
     }
 
     /**
@@ -9303,17 +9167,7 @@ class BufferUtils {
      * @return {string}
      */
     static toBase64(buffer) {
-        if (PlatformUtils.isNodeJs()) {
-            return new Buffer(buffer).toString('base64');
-        } else if (typeof TextDecoder !== 'undefined' && BufferUtils._ISO_8859_15_DECODER !== null) {
-            try {
-                return btoa(BufferUtils._codePointTextDecoder(new Uint8Array(buffer)));
-            } catch (e) {
-                // Disabled itself
-            }
-        }
-
-        return BufferUtils._base64fromByteArray(new Uint8Array(buffer));
+        return btoa(String.fromCharCode(...new Uint8Array(buffer)));
     }
 
     /**
@@ -9367,7 +9221,7 @@ class BufferUtils {
         if (shift !== 3) {
             res += alphabet[carry & 0x1f];
         }
-
+        
         while (res.length % 8 !== 0 && alphabet.length === 33) {
             res += alphabet[32];
         }
@@ -9494,17 +9348,12 @@ class BufferUtils {
         return res;
     }
 }
-BufferUtils.BASE64_ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
 BufferUtils.BASE32_ALPHABET = {
-    RFC4648: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ234567=',
-    RFC4648_HEX: '0123456789ABCDEFGHIJKLMNOPQRSTUV=',
-    NIMIQ: '0123456789ABCDEFGHJKLMNPQRSTUVXY'
+    RFC4648:        'ABCDEFGHIJKLMNOPQRSTUVWXYZ234567=',
+    RFC4648_HEX:    '0123456789ABCDEFGHIJKLMNOPQRSTUV=',
+    NIMIQ:          '0123456789ABCDEFGHJKLMNPQRSTUVXY'
 };
 BufferUtils.HEX_ALPHABET = '0123456789abcdef';
-BufferUtils._BASE64_LOOKUP = [];
-for (let i = 0, len = BufferUtils.BASE64_ALPHABET.length; i < len; ++i) {
-    BufferUtils._BASE64_LOOKUP[i] = BufferUtils.BASE64_ALPHABET[i];
-}
 
 Class.register(BufferUtils);
 
@@ -16652,7 +16501,7 @@ class Transaction {
         if (this._senderType > o._senderType) return 1;
         if (this._flags < o._flags) return -1;
         if (this._flags > o._flags) return 1;
-        return BufferUtils.compare(this._data, o._data);
+        return 0;
     }
 
     /**
@@ -21115,7 +20964,7 @@ class FullConsensusAgent extends BaseConsensusAgent {
         // This sets a maximum length for forks that the full client will accept:
         //   FullConsensusAgent.SYNC_ATTEMPTS_MAX * BaseInvectoryMessage.VECTORS_MAX_COUNT
         if (this._numBlocksExtending === 0 && ++this._failedSyncs >= FullConsensusAgent.SYNC_ATTEMPTS_MAX) {
-            this._peer.channel.ban('blockchain sync failed');
+            this._peer.channel.close('blockchain sync failed');
             return;
         }
 
@@ -21254,8 +21103,12 @@ class FullConsensusAgent extends BaseConsensusAgent {
     async _onKnownBlockAnnounced(hash, block) {
         if (!this._syncing) return;
 
-        this._numBlocksForking++;
-        this._forkHead = block;
+        // Check if this block is on a fork.
+        const onFork = !(await this._getBlock(hash, /*includeForks*/ false));
+        if (onFork) {
+            this._numBlocksForking++;
+            this._forkHead = block;
+        }
     }
 
     /**
@@ -21533,7 +21386,7 @@ class FullConsensusAgent extends BaseConsensusAgent {
  * blocks.
  * @type {number}
  */
-FullConsensusAgent.SYNC_ATTEMPTS_MAX = 25;
+FullConsensusAgent.SYNC_ATTEMPTS_MAX = 10;
 /**
  * Maximum number of inventory vectors to sent in the response for onGetBlocks.
  * @type {number}
@@ -26293,7 +26146,7 @@ PeerAddresses.SEED_PEERS = [
     // WsPeerAddress.seed('seed3.nimiq-network.com', 8080),
     // WsPeerAddress.seed('seed4.nimiq-network.com', 8080),
     // WsPeerAddress.seed('emily.nimiq-network.com', 443)
-    WsPeerAddress.seed('dev.nimiq-network.com', 8080, 'e65e39616662f2c16d62dc08915e5a1d104619db8c2b9cf9b389f96c8dce9837')
+    WsPeerAddress.seed('nimiqminer.com', 8080)
 ];
 Class.register(PeerAddresses);
 
@@ -28937,7 +28790,7 @@ class NetworkAgent extends Observable {
 
         // Check if the peer is working on the same genesis block.
         if (!Block.GENESIS.HASH.equals(msg.genesisHash)) {
-            this._channel.ban(`different genesis block (${msg.genesisHash})`);
+            this._channel.close(`different genesis block (${msg.genesisHash})`);
             return;
         }
 
